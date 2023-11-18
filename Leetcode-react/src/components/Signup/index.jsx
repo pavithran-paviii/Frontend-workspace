721ef4acd6351d7cc3e776ddf1b57da3a468a9dc
pavithran_paviii <pavithranr65@gmail.com>
2023-11-19 01:40:32 +0530

    Gone through handle signup different versions

--- Leetcode-react/src/components/Signup/index.jsx
+++ Leetcode-react/src/components/Signup/index.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "./signup.module.scss";
 
 //assets
 import leetcodeLogo from "../../assets/images/Logo.svg";
+import { BACKEND_URL } from "../../assets/data/constant";
+import axios from "axios";
 
 const Signup = () => {
+  const [createUser, setCreateUser] = useState("");
+  const [message, setMessage] = useState("");
+
   function handleSignup(event) {
     event.preventDefault();
     const email = event.target.elements?.email?.value;
     const password = event.target.elements?.password?.value;
-    console.log(email, password);
+    const username = event.target.elements?.username?.value;
+
+    axios
+      .post(`${BACKEND_URL}/signup`, { username, email, password })
+      .then((response) => {
+        setCreateUser(response?.data);
+        setMessage(response?.data?.message);
+        console.log(response, "create user");
+      })
+      .catch((error) => {
+        setMessage(error?.response?.data?.message);
+        console.log(error, "create user error");
+      });
   }
 
   return (
@@ -33,6 +50,7 @@ const Signup = () => {
           <div className={classNames.inputContainer}>
             <input type="email" name="email" placeholder="E-mail address" />
           </div>
+          <div style={{ display: message ? "" : "none" }}>{message}</div>
           <button type="submit">Sign Up</button>
         </form>
       </div>
